628ceb9ada9274b018ccc9dbbd1e89c022c586de
Franz Graf <code@Locked.de>
2016-01-08 23:10:48 +0100

    streams

--- src/main/java/de/locked/game2048/BoardController.java
+++ src/main/java/de/locked/game2048/BoardController.java
@@ -1,14 +1,25 @@
 package de.locked.game2048;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class BoardController {
+
+    public enum STATE {
+        WON, LOOSE, PENDING;
+    }
 
     private final Cell[] board;
     private final int w = 4;
     private final int h = 4;
+    private final int WINNING_VALUE = 2048;
+    private STATE state = STATE.PENDING;
     private int score = 0;
 
     public BoardController() {
@@ -16,50 +27,45 @@ class BoardController {
         for (int i = 0; i < board.length; i++) {
             board[i] = Cell.EMPTY;
         }
-        addNewCell(2);
+        addNewCell();
+        addNewCell();
     }
 
     public int getScore() {
         return score;
     }
 
-    private int index(int col, int row) {
-        return row * w + col;
-    }
-
     void right() {
-        int moves = 0;
+        boolean[] processed = new boolean[board.length];
         for (int row = 0; row < h; row++) {
-            int columnProcessed = w;
             for (int col = w - 2; col >= 0; col--) {
                 if (!board[index(col, row)].isEmpty()) {
-                    int from = col;
+                    int fromCol = col;
                     boolean proceed = true;
-                    while (proceed && from + 1 < columnProcessed) {
-                        int src = index(from, row);
-                        int dst = index(from + 1, row);
+                    while (proceed && !processed[index(col + 1, row)]) {
+                        int src = index(fromCol, row);
+                        int dst = index(fromCol + 1, row);
 
-                        if (board[dst].isEmpty()) {
+                        if (board[dst].isEmpty()) { // move to right
                             board[dst] = board[src];
                             board[src] = Cell.EMPTY;
-                            moves++;
                         } else {
                             if (board[dst].canMergeWith(board[src])) {
                                 board[dst] = board[dst].mergeWith(board[src]);
                                 board[src] = Cell.EMPTY;
-                                moves++;
+                                score += board[dst].getValue();
                             }
                             proceed = false;
                         }
-                        from++;
+                        fromCol++;
                     }
+                    processed[index(fromCol - 1, row)] = true;
                 }
             }
         }
 
-        if (moves > 0) {
-            addNewCell();
-        }
+        addNewCell();
+        updateState();
     }
 
     void top() {
@@ -94,23 +100,53 @@ class BoardController {
         return s;
     }
 
-    private void addNewCell() {
-        int value = Math.random() < 0.9 ? 2 : 4;
-        addNewCell(value);
+    private int newValue() {
+        return Math.random() < 0.9 ? 2 : 4;
     }
 
-    private int getFreeCellIndex() {
-        List<Integer> candidates = new ArrayList<>();
+    private void addNewCell() {
+        getFreeCellIndex().ifPresent(i -> board[i] = new Cell(newValue()));
+    }
+
+    private Optional<Integer> getFreeCellIndex() {
+        int[] candidates = IntStream.range(0, board.length)
+                .filter(i -> board[i].isEmpty())
+                .toArray();
+        final int size = candidates.length;
+        return size == 0 ? Optional.empty() : Optional.of(candidates[new Random().nextInt(size)]);
+    }
+
+    private int index(int col, int row) {
+        return row * w + col;
+    }
+
+    private boolean canContinue() {
+        return getFreeCellIndex().isPresent() || canMerge();
+    }
+
+    private boolean canMergeIndex(int a, int b) {
+        if (a < 0 || a >= board.length || b < 0 || b >= board.length) {
+            return false;
+        } else {
+            return board[a].canMergeWith(board[b]);
+        }
+    }
+
+    private boolean canMerge() {
+        // just check against right & bottom as we're symmetric
         for (int i = 0; i < board.length; i++) {
-            if (board[i].isEmpty()) {
-                candidates.add(i);
+            if (canMergeIndex(i, i + 1) || canMergeIndex(i, i + w)) {
+                return true;
             }
         }
-        return new Random().nextInt(candidates.size());
+        return false;
     }
 
-    private void addNewCell(int value) {
-        board[getFreeCellIndex()] = new Cell(value);
+    private void updateState() {
+        if (Arrays.stream(board).anyMatch(c -> c.getValue() == WINNING_VALUE)) {
+            state = STATE.WON;
+        } else if (!canContinue()) {
+            state = STATE.LOOSE;
+        }
     }
-
 }
