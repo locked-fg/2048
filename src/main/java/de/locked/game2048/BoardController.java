da607d877417f4f76280aecca50a4480f8ccc9d0
Franz Graf <code@Locked.de>
2016-01-09 11:43:23 +0100

    okay moving right seems to work :D

--- src/main/java/de/locked/game2048/BoardController.java
+++ src/main/java/de/locked/game2048/BoardController.java
@@ -2,12 +2,13 @@ package de.locked.game2048;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class BoardController {
 
@@ -18,6 +19,7 @@ class BoardController {
     private final Cell[] board;
     private final int w = 4;
     private final int h = 4;
+    private final Board boardM;
     private final int WINNING_VALUE = 2048;
     private STATE state = STATE.PENDING;
     private int score = 0;
@@ -27,8 +29,9 @@ class BoardController {
         for (int i = 0; i < board.length; i++) {
             board[i] = Cell.EMPTY;
         }
-        addNewCell();
-        addNewCell();
+        boardM = new Board(w, h, board);
+        boardM.addNewCell();
+        boardM.addNewCell();
     }
 
     public int getScore() {
@@ -36,36 +39,10 @@ class BoardController {
     }
 
     void right() {
-        boolean[] processed = new boolean[board.length];
-        for (int row = 0; row < h; row++) {
-            for (int col = w - 2; col >= 0; col--) {
-                if (!board[index(col, row)].isEmpty()) {
-                    int fromCol = col;
-                    boolean proceed = true;
-                    while (proceed && !processed[index(col + 1, row)]) {
-                        int src = index(fromCol, row);
-                        int dst = index(fromCol + 1, row);
-
-                        if (board[dst].isEmpty()) { // move to right
-                            board[dst] = board[src];
-                            board[src] = Cell.EMPTY;
-                        } else {
-                            if (board[dst].canMergeWith(board[src])) {
-                                board[dst] = board[dst].mergeWith(board[src]);
-                                board[src] = Cell.EMPTY;
-                                score += board[dst].getValue();
-                            }
-                            proceed = false;
-                        }
-                        fromCol++;
-                    }
-                    processed[index(fromCol - 1, row)] = true;
-                }
-            }
+        if (move(Move.RIGHT)) {
+            boardM.addNewCell();
+            updateState();
         }
-
-        addNewCell();
-        updateState();
     }
 
     void top() {
@@ -78,6 +55,59 @@ class BoardController {
 
     void bottom() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean move(Move move) {
+        boolean[][] merged = new boolean[w][h];
+        boolean moved = false;
+        for (Coord src : buildMoves(move)) {
+            Coord dst = findTargetCell(src, move, merged);
+            if (!src.equals(dst)) {
+                if (boardM.isEmpty(dst)) {
+                    boardM.move(src, dst);
+                } else { // merge
+                    boardM.merge(src, dst);
+                    score += boardM.get(dst).getValue();
+                    merged[dst.x][dst.y] = true;
+                }
+                moved = true;
+            }
+        }
+        return moved;
+    }
+
+    private Coord findTargetCell(final Coord src, Move m, boolean[][] merged) {
+        Coord now, next = src;
+        do {
+            now = next;
+            next = now.step(m);
+        } while (boardM.inBounds(next) && boardM.isEmpty(next));
+
+        // next is now out of bounds OR occupied (and maybe mergable)
+        if (!boardM.inBounds(next)) {
+            return now;
+        } else if (!boardM.canMerge(src, next) || merged[next.x][next.y]) {
+            // non mergable or merged already in this move
+            return now;
+        } else {
+            return next;
+        }
+    }
+
+    private List<Coord> buildMoves(Move move) {
+        List<Coord> toMove = new ArrayList<>();
+        for (int col = 0; col < w; col++) {
+            for (int row = 0; row < h; row++) {
+                toMove.add(new Coord(col, row));
+            }
+        }
+        //always start from the far side
+        if (move == Move.DOWN) {
+            Collections.sort(toMove, (c1, c2) -> -Integer.compare(c1.y, c2.y));
+        } else if (move == Move.RIGHT) {
+            Collections.sort(toMove, (c1, c2) -> -Integer.compare(c1.x, c2.x));
+        }
+        return Collections.unmodifiableList(toMove);
     }
 
     @Override
@@ -100,52 +130,14 @@ class BoardController {
         return s;
     }
 
-    private int newValue() {
-        return Math.random() < 0.9 ? 2 : 4;
-    }
-
-    private void addNewCell() {
-        getFreeCellIndex().ifPresent(i -> board[i] = new Cell(newValue()));
-    }
-
-    private Optional<Integer> getFreeCellIndex() {
-        int[] candidates = IntStream.range(0, board.length)
-                .filter(i -> board[i].isEmpty())
-                .toArray();
-        final int size = candidates.length;
-        return size == 0 ? Optional.empty() : Optional.of(candidates[new Random().nextInt(size)]);
-    }
-
     private int index(int col, int row) {
         return row * w + col;
-    }
-
-    private boolean canContinue() {
-        return getFreeCellIndex().isPresent() || canMerge();
-    }
-
-    private boolean canMergeIndex(int a, int b) {
-        if (a < 0 || a >= board.length || b < 0 || b >= board.length) {
-            return false;
-        } else {
-            return board[a].canMergeWith(board[b]);
-        }
-    }
-
-    private boolean canMerge() {
-        // just check against right & bottom as we're symmetric
-        for (int i = 0; i < board.length; i++) {
-            if (canMergeIndex(i, i + 1) || canMergeIndex(i, i + w)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private void updateState() {
         if (Arrays.stream(board).anyMatch(c -> c.getValue() == WINNING_VALUE)) {
             state = STATE.WON;
-        } else if (!canContinue()) {
+        } else if (!boardM.canContinue()) {
             state = STATE.LOOSE;
         }
     }
